dd028e8119a7973c0f5e4a171f9cc9ee3f853233
ruben787678-dot <ruben787678@gmail.com>
2026-05-20 16:32:42 +0000

    save

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from os import getenv
 import asyncio 
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+
 TOKEN= getenv("BOT_TOKEN")
+
+dp = Dispatcher()
+
+async def main():
+    bot= Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ =="__main__":
+    asyncio.run(main())
+    
+
+
+
 
